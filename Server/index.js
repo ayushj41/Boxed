42e72637a4d001c59b51cd5d4aebe49fc4e19715
Piyushjha03 <jhapiyush2305@gmail.com>
2025-02-07 03:34:59 +0530

    Refactor server setup: move bodyParser middleware and export app instance

--- Server/index.js
+++ Server/index.js
@@ -9,12 +9,13 @@ const app = express();
 const PORT = 3000;
 
 // Middleware
+app.use(bodyParser.json());
+
 app.use(
   cors({
     origin: "*",
   })
 );
-app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose
@@ -367,3 +368,5 @@ app.use("/ai", AiRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+export default app;
